28b7c4a496e8b19a396f4f8ad57bc11f9f4d37c1
Justin Han <jdhan@cpp.edu>
2018-03-31 07:28:00 -0700

    Added coach and trainee page

--- SwolMate/App.js
+++ SwolMate/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, TextInput, Button, View, Text } from 'react-native';
+import StarRatingBar from 'react-native-star-rating-view/StarRatingBar'
+import { Image, StyleSheet, TextInput, Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
 class SignUp extends React.Component {
@@ -22,12 +23,14 @@ class SignUp extends React.Component {
           <TextInput
             style={signUpStyle.container}
             placeholder="Email"
+            keyboardType={'email-address'}
             onChangeText={(email) => this.setState({email})}
             value={this.state.email}
           />
           <TextInput
             style={signUpStyle.container}
             placeholder="Password"
+            secureTextEntry={true}
             onChangeText={(password) => this.setState({password})}
             value={this.state.password}
           />
@@ -45,7 +48,7 @@ class Login extends React.Component {
   super(props);
   this.state = {  email: '',
                   password: ''};
-}
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -53,12 +56,14 @@ class Login extends React.Component {
           <TextInput
             style={signUpStyle.container}
             placeholder="Email"
+            keyboardType={'email-address'}
             onChangeText={(email) => this.setState({email})}
             value={this.state.email}
           />
           <TextInput
             style={signUpStyle.container}
             placeholder="Password"
+            secureTextEntry={true}
             onChangeText={(password) => this.setState({password})}
             value={this.state.password}
           />
@@ -66,11 +71,225 @@ class Login extends React.Component {
           title="Login"
           onPress={() => this.props.navigation.navigate('Login')}
         />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
       </View>
+    );
+  }
+}
+
+class Coach extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  name: '',
+                  gym: '',
+                  location: '',
+                  pay: '',
+                  rate: 2.7,
+                  bio: '',
+                  filters: ''};
+
+  }
+
+  render() {
+    return (
+      //card view
+      <View style={{flexDirection: 'column'}}>
+        //image
+        <View style={{alignItems: 'center'}}>
+          <Image
+            style={{width: 50, height: 50}}
+            source={{uri: 'https://pbs.twimg.com/profile_images/344513261579284169/33463ce370b5e1702393ca16a4cad5e6_400x400.jpeg'}}
+          />
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //info
+        <View style={{flexDirection: 'column'}}>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Name</Text>
+            <Text>
+              {this.state.name}
+            </Text>
+            <Button
+              title="Edit"
+              onPress={() => this.props.navigation.navigate('Edit')}
+            />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Gym</Text>
+              <Text>
+                {this.state.gym}
+              </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Location</Text>
+              <Text>
+                {this.state.location}
+              </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //pay rate and rating
+        <View style={{flexDirection: 'column'}}>
+          //pay rate
+          <View style={{flexDirection: 'row'}}>
+            <Text>Pay Rate</Text>
+              <Text>
+                {this.state.pay}
+              </Text>
+          </View>
+          //rating
+          <StarRatingBar
+            score={this.state.rate}
+            allowsHalfStars={true}
+            accurateHalfStars={true}
+          />
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //Bio
+        <View style={{flexDirection: 'column'}}>
+          <Text>Bio</Text>
+          <Text>{this.state.bio}</Text>
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //Filter
+        <View style={{flexDirection: 'column'}}>
+          <Text>Filter</Text>
+          <Text>{this.state.filter}</Text>
+        </View>
+      </View>
+
+
+    );
+  }
+}
+
+class Trainee extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  name: '',
+                  gym: '',
+                  location: '',
+                  goal: '',
+                  bio: '',
+                  filters: ''};
+
+  }
+
+  render() {
+    return (
+      //card view
+      <View style={{flexDirection: 'column', justifyContent: 'space-between'}}>
+        //image
+        <View>
+          <Image
+            style={{width: 50, height: 50}}
+            source={{uri: 'https://pbs.twimg.com/profile_images/344513261579284169/33463ce370b5e1702393ca16a4cad5e6_400x400.jpeg'}}
+          />
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //info
+        <View style={{flexDirection: 'column'}}>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Name</Text>
+            <Text>
+              {this.state.name}
+            </Text>
+            <Button
+              title="Edit"
+              onPress={() => this.props.navigation.navigate('Edit')}
+            />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Gym</Text>
+              <Text>
+                {this.state.gym}
+              </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text>Location</Text>
+              <Text>
+                {this.state.location}
+              </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //fitness goal
+        <View style={{flexDirection: 'column'}}>
+            <Text>Fitness Goal</Text>
+            <Text>
+              {this.state.goal}
+            </Text>
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //Bio
+        <View style={{flexDirection: 'column'}}>
+          <Text>Bio</Text>
+          <Text>{this.state.bio}</Text>
+        </View>
+
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+          }}
+        />
+
+        //Filter
+        <View style={{flexDirection: 'column'}}>
+          <Text>Filter</Text>
+          <Text>{this.state.filter}</Text>
+        </View>
+      </View>
+
+
     );
   }
 }
@@ -83,9 +302,15 @@ const RootStack = StackNavigator(
     Login: {
       screen: Login,
     },
+    Coach: {
+      screen: Coach,
+    },
+    Trainee: {
+      screen: Trainee,
+    },
   },
   {
-    initialRouteName: 'SignUp',
+    initialRouteName: 'Coach',
   }
 );
 
